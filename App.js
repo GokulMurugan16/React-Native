b7474e256e0d949e02c46501ccb9a5db1183b083
GokulMurugan16 <Gokul16.619@gmail.com>
2021-01-10 20:09:47 -0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, Button } from 'react-native';
 
 export default function App() {
+const [name, setname] = useState("")
+const [dispNAme, setDispName] = useState("Nothin")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source ={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}} style={{width:200, height:200 }}/>
+      <TextInput onChangeText = {name => setname(name)}  placeholder = "Type Here" style = {{textAlign:"center", borderColor:'gray', borderWidth:2, borderRadius:10, padding:20, marginBottom:20}}/>
+      <Button onPress={()=>{
+        setDispName(name)
+      }}
+      title = "Change Text"/>
+
+      <Text>
+        {name}
+      </Text>
+      <StatusBar/>
     </View>
   );
 }
