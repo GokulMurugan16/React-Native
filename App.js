04dae627382b2dda6b44cb566726f449f2e15b7a
GokulMurugan16 <Gokul16.619@gmail.com>
2021-01-10 20:14:59 -0500

    Test

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ const [name, setname] = useState("")
 const [dispNAme, setDispName] = useState("Nothin")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <Image source ={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}} style={{width:200, height:200 }}/>
       <TextInput onChangeText = {name => setname(name)}  placeholder = "Type Here" style = {{textAlign:"center", borderColor:'gray', borderWidth:2, borderRadius:10, padding:20, marginBottom:20}}/>
       <Button onPress={()=>{
